0716cbd5669565608ad124a0e8e98a8e8cf68c72
Akshat Jain <121884340+fiercfly@users.noreply.github.com>
2025-01-13 22:51:36 +0530

    Update CartProduct.js

--- frontend/src/component/CartProduct.js
+++ frontend/src/component/CartProduct.js
@@ -24,7 +24,7 @@ const CartProduct = ({ id, name, image, category, qty, total, price }) => {
 
         <p className='text-slate-500 font-medium'>{category}</p>
         <p className='font-bold text-base'>
-          <span className='text-red-500'>$</span>
+          <span className='text-red-500'>₹</span>
           <span>{price}</span>
         </p>
         <div className='flex justify-between'>
