05c57b9b5b7a33d884eec587fe28f3d725fb7678
Akshat Jain <121884340+fiercfly@users.noreply.github.com>
2025-01-13 23:07:05 +0530

    Update HomeCard.js

--- frontend/src/component/HomeCard.js
+++ frontend/src/component/HomeCard.js
@@ -14,7 +14,7 @@ const HomeCard = ({name, image, category, price, loading, id}) => {
                     </div>
                     <h3 className='font-semibold text-slate-600 text-center capitalize text-lg'>{name}</h3>
                     <p className='text-center text-slate-500 font-medium'>{category}</p>
-                    <p className= 'text-center font-bold'><span className='text-red-500'>$</span><span>{price}</span></p>
+                    <p className= 'text-center font-bold'><span className='text-red-500'>₹</span><span>{price}</span></p>
                 </Link>
                 </>
             )
